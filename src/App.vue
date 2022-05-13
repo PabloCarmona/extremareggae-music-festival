dd409ff65f1a89ff7419320d89e2dec0f7bbc351
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-05-13 17:26:59 +0200

    Add WIP sections to dev branch

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import NavBar from './components/NavBar.vue';
 import Hero from './components/Hero.vue'
+import Cartel from './components/Cartel.vue';
+import AboutUs from './components/AboutUs.vue';
+import Artists from './components/Artists.vue';
+import Artist from './components/Artist.vue';
+
+const BLACK_OMOLO_RRSS = [
+  { name: 'Instagram', link: 'https://www.instagram.com/blackomolo/', icon: '/src/assets/instagram.svg' },
+  { name: 'Spotify', link: 'https://open.spotify.com/artist/2l3Pw2e2c6xlA2gmeXJThH?si=y784JulIQ6mgB4EYdYTtJw&nd=1', icon: '/src/assets/spotify.svg' }
+]
 </script>
 
 <template>
+  <NavBar />
   <Hero />
+  <Cartel />
+  <AboutUs />
+  <Artists />
+  <Artist backgroundImageUrl="/src/assets/black-omolo.jpg" name="Black Omolo<br>&amp; Militanti Band"
+    country="(Kenia - Portugal)" :socialMediaInfo="BLACK_OMOLO_RRSS" fontColor="#fff" />
+  <Artist backgroundImageUrl="/src/assets/gene-garcia.jpg" name="Gene Garc&iacute;a &amp; The Rockets"
+    country="(Espa&ntilde;a)" :socialMediaInfo="BLACK_OMOLO_RRSS" fontColor="#212121" />
 </template>
 
 <style>
@@ -13,6 +31,7 @@ body {
   padding: 0;
   margin: 0;
 }
+
 #app {
   font-family: 'Gotham', sans-serif;
   -webkit-font-smoothing: antialiased;
